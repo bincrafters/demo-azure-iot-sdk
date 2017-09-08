928f5aec7f8cb4afbdd1bd5dfced54774be87ef3
Jerry W <jerrywiltse@gmail.com>
2017-09-07 21:07:38 -0400

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile
+
+
+class AzureIotSdkDemoConan(ConanFile):
+    name = "Azure-IOT-SDK-Demo-Conan"
+    version = "1.0.0"
+    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    url = "https://github.com/bincrafters/azure-iot-sdk-demo-conan"
+    license = "https://github.com/bincrafters/azure-iot-sdk-demo-conan/blob/master/LICENSE"
+    requires = "Azure-IoT-SDK-C/1.1.21@bincrafters/testing"
+
+    def configure(self):
+        self.options["Azure-C-Shared-Utility"].shared = False
